b5abf5878a937cfbfd915adb08d4f10821e016d0
Andrew Siak <140351152+andrewsiak@users.noreply.github.com>
2024-03-22 11:41:57 -0400

    creating leetcode git folder and two sums solution

--- leetcode/leetcode_twosums.py
+++ leetcode/leetcode_twosums.py
@@ -24,5 +24,14 @@ sum_test = 16
 test_list = [1,2,3,4,5,6,7,8,9]
 print("Test", two_sum_dict(test_list, sum_test))
 
+test_case_one = [2,7,11,15]
+sum_one = 9
+print("Case 1", two_sum_dict(test_case_one, sum_one))
 
+test_case_two = [3,2,4]
+sum_two = 6
+print("Case 2", two_sum_dict(test_case_two, sum_two))
 
+test_case_three = [3,3]
+sum_three = 6
+print("Case 3", two_sum_dict(test_case_three, sum_three))
